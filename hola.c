e0a2b30c453cf1e0d2e6ab41d494317fda04faed
Ulises Juarez <mmcjuarez@gmail.com>
2025-05-27 19:47:03 -0600

    Suma

--- hola.c
+++ hola.c
@@ -5,7 +5,7 @@ int main() {
     printf("\nComo estas?");
 }
 
-int factorial(int n){
-    if(n==0) return 1;
-    return n* factorial(n-1);
+int suma(int n){
+    if(n==0) return 0;
+    return n + suma(n-1);
 }
